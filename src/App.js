1646646f8bfd59a4d37b04e6dea0f3d1dd85da5d
rithick-11 <rithickroshan7878@gmail.com>
2024-07-28 09:16:21 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section>
+      <div>
+        <h1>rithick protfolio</h1>
+      </div>
+    </section>
+  )
 }
 
-export default App;
+export default App
